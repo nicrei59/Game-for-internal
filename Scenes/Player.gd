3a12c3afb783f7f91da31ccef853be2c4a17edda
nicrei59 <nicrei59@kingshigh.school.nz>
2024-08-21 10:28:13 +1200

    1.1 version
    Movement done

---
+++ Scenes/Player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+var speed = 200
+var velocity = Vector2()
+
+
+func _physics_process(delta):
+	velocity = Vector2()
+	
+	if Input.is_action_just_pressed("Down"):
+		velocity.y += speed
+	if Input.is_action_just_pressed("Up"):
+		velocity.y -= speed
+	if Input.is_action_just_pressed("Left"):
+		velocity.x -= speed 
+	if Input.is_action_just_pressed("Right"):
+		velocity.x += speed
+	
+	move_and_slide(velocity)
+	look_at(get_global_mouse_position())
+
+  
